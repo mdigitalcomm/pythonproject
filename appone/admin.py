85bd5191e894c6a3f48ab33f369b8ad3cb945cd0
mdigitalcomm <mdigitalcomm@gmail.com>
2019-08-18 17:02:20 -0400

    Added and registered

--- appone/admin.py
+++ appone/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Topic, Website
 # Register your models here.
+
+admin.site.register(Topic)
+admin.site.register(Website)
--- appone/models.py
+++ appone/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    topic_name = models.CharField(max_length=246, unique=True)
+
+class Website(models.Model):
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        )
+    web_name = models.CharField(max_length=246, unique=True)
+    url = models.URLField()
